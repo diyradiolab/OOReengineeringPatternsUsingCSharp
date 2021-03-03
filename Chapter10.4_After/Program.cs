fea403435529658d0ea4906b217f0785dc48f002
diyradiolab <diyradiolab@gmail.com>
2021-03-03 12:29:41 -0500

    Add project files.

---
+++ Chapter10.4_After/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter10._4_After
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Message myMessage = new Action_Message(4);
+            //Message myMessage = new Text_Message("Some Text");  <--invoke if you want to send a textmessage
+
+            var myChannel = new Channel();
+            myMessage.Send(myChannel);
+            Console.ReadLine();
+        }
+
+        public abstract class Message
+        {
+            public abstract void Send(Channel channel);
+        }
+
+        public class Text_Message : Message
+        {
+            public string Text;
+            public Text_Message(string text)
+            {
+                Text = text;
+            }
+            public override void Send(Channel c) { Console.WriteLine("Sending text to a machine..."); }
+
+        }
+
+        public class Action_Message : Message
+        {
+            public int Action;
+            public Action_Message(int action)
+            {
+                Action = action;
+            }
+            public override void Send(Channel c) { Console.WriteLine("Sending an action to a machine..."); }
+        }
+
+        public class Channel
+        {
+        }
+
+    }
+}
---
+++ Chapter10.4_Before/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter10._4_Before
+{
+    class Program
+    {
+      
+        static void Main(string[] args)
+        {
+
+
+
+            var myMessage = new Message();
+            myMessage.set_value("Text"); // here you can send a string or an int. Depending on the type, it will set the type_ property to 1 or 2, which will cause a different format of message to be sent by Message.Send
+            var myChannel = new Channel();
+            myMessage.Send(myChannel);
+            Console.ReadLine();
+
+        }
+
+    }
+
+    public class Message
+    {
+
+        public int type_;
+
+        public Message()
+        {
+            //constructor
+        }
+
+        public void set_value(int action) { type_ = 2; } //text type message
+
+        public void set_value(string text) { type_ = 1; } //action type message
+
+        public void Send(Channel c)
+        {
+
+            switch (type_)
+            {
+                case 1:
+                    Console.WriteLine("Sending text to a machine...");
+
+
+                    break;
+                case 2:
+                    Console.WriteLine("Sending an action to a machine...");
+                    break;
+                default: break;
+            }
+
+        }
+    }
+
+    public class Channel
+    {
+    }
+}
